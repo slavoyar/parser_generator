15ed278166551e3a668526d06ba382533fb4d167
slavoyar <slavoyarmc@gmail.com>
2022-05-25 11:39:24 +0300

    Generating rules array without its names.

--- src/Parser/Parser.ts
+++ src/Parser/Parser.ts
@@ -1,10 +1,14 @@
 import { GrammarTable, Relation } from "../Grammar";
 import { Tokenizer } from "../Tokenizer";
 
+/**
+ * Class for parsing input file.
+ */
 export class Parser {
   private _tokenizer: Tokenizer
-  private _grammarTable: GrammarTable
-  private _tokenDefenition: Object = {
+  private _namelessRules: string[][]
+  private _precedenceTable: GrammarTable
+  private _grammarTokens: Object = {
     'p': /[a-zA-Z]+/,
     'AND': /\&/,
     'XOR': /\^/,
@@ -13,6 +17,13 @@ export class Parser {
     'RIGHT_PR': /\)/,
     '_': /\s+|\t+|\n+/
   }
+  private _grammarRules = {
+    'S': ['MINUS B'],
+    'B': ['T', 'B AND T'],
+    'T': ['J', 'T XOR J'],
+    'J': ['p', 'LEFT_PR B RIGHT_PR']
+  }
+
   private _table: Object = {
     p: { p: 3, AND: 2, XOR: 2, MINUS: 3, LEFT_PR: 3, RIGHT_PR: 2 },
     AND: { p: 0, AND: 2, XOR: 0, MINUS: 3, LEFT_PR: 0, RIGHT_PR: 2 },
@@ -25,14 +36,20 @@ export class Parser {
   /**
    * Construct instance of parser class.
    * 
-   * @param file File that will be analised
+   * @param file File that will be analysed.
    */
   constructor(private file: string) {
-    this._tokenizer = new Tokenizer(this._tokenDefenition)
-    this._grammarTable = this.objToTable()
-    console.log(this._grammarTable)
+    this._tokenizer = new Tokenizer(this._grammarTokens)
+    this._precedenceTable = this.objToTable()
+    this._namelessRules = this.generateNamelessRules()
+    console.log(this._namelessRules)
   }
 
+  /**
+   * Create object of GrammarType from js object.
+   * 
+   * @returns Grammar precedence table.
+   */
   private objToTable(): GrammarTable {
     let result: GrammarTable = new Map<string, Map<string, Relation>>()
     const rows = new Map<string, Object>(Object.entries(this._table))
@@ -41,5 +58,28 @@ export class Parser {
     }
     return result
   }
-  
+
+  /**
+   * Generates nameless rules array from input grammar rules.
+   * 
+   * @returns Rules without names specified for them.
+   */
+  private generateNamelessRules(): string[][] {
+    let result: string[][] = []
+    const rules = new Map<string, string[]>(Object.entries(this._grammarRules))
+    const ruleNames = Array.from(rules.keys())
+    for (const [_, value] of rules) {
+      value.forEach(el => {
+        let tempArr = el.split(' ')
+        let rule: string[] = []
+        tempArr.forEach(symbol => {
+          symbol = ruleNames.includes(symbol) ? 'E' : symbol
+          rule.push(symbol)
+        })
+        result.push(rule)
+      })
+    }
+    return result
+  }
+
 }
